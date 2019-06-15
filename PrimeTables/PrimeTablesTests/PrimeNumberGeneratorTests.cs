45e7923ccc4692cc8d7b3282c5e704b67eeeb6f8
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-15 14:51:26 +0100

    write initial prime number generation tests (failing)

--- PrimeTables/PrimeTablesTests/PrimeNumberGeneratorTests.cs
+++ PrimeTables/PrimeTablesTests/PrimeNumberGeneratorTests.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using PrimeTables;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace PrimeTablesTests
 {
     public class PrimeNumberGeneratorTests
     {
+
+        [Fact]
+        public void GeneratePrimeNumbers_Not_Empty_Or_Null()
+        {
+            var primes = PrimeNumberGenerator.GeneratePrimeNumbers(5);
+            Assert.NotEmpty(primes);
+            Assert.NotNull(primes);
+        }
+
+        [Fact]
+        public void GeneratePrimeNumbers_Expected_Output()
+        {
+            var expectedPrimes = new List<int>() { 2,3,5,7,11 };
+            var primes = PrimeNumberGenerator.GeneratePrimeNumbers(5);
+            Assert.Equal(expectedPrimes, primes);
+        }
        
     }
 }
