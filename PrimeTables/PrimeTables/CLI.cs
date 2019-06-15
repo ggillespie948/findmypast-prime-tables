8c992f856334204b35dddd128628c8d09b57cab7
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-15 22:32:18 +0100

    Add CLI unit tests (argument validation) and CLI initial implementation

--- PrimeTables/PrimeTables/CLI.cs
+++ PrimeTables/PrimeTables/CLI.cs
@@ -2,11 +2,83 @@
 
 namespace PrimeTables
 {
-    class CLI
+    public class CLI
     {
+        private static readonly int[] PRIME_NUMBERS;
+
         static void Main(string[] args)
         {
-            
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Welcome to prime number tables.");
+                Console.WriteLine("Please enter the number of prime multiple you would like to generate (1 or greater) :");
+                args = new string[2];
+                args[0] = "-n";
+                args[1] = Console.ReadLine();
+            } 
+
+            switch (args[0])
+            {
+                case "-n":
+                    if (args.Length >= 2)
+                    {
+                        if (ValidateNumberArgument(args[1], out int nPrime))
+                        {
+                            var primeNumbers = PrimeNumberGenerator.GeneratePrimeNumbers(nPrime);
+                            // pass this to multiplication output table
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: N primes argument provided. Please provide a whole number great than 1");
+                            return;
+                        }
+                    }
+                    break;
+
+                case "-help":
+                    PrintHelpInstructions();
+                    break;
+
+                default:
+                    PrintHelpInstructions();
+                    break;
+
+            }
+        }
+
+        /// <summary>
+        /// Method which parses the N argument from the CLI, validates the result
+        /// and returns it as an integer using an out variable
+        /// </summary>
+        /// <param name="Nstr"></param>
+        /// <param name="nPrime"></param>
+        /// <returns></returns>
+        public static bool ValidateNumberArgument(string Nstr, out int nPrime)
+        {
+            if (int.TryParse(Nstr, out nPrime))
+            {
+                if (!(nPrime % 1 == 0))
+                {
+                    Console.Write("Error: please provide a whole number");
+                    return false;
+                }
+                else if (nPrime < 1)
+                {
+                    Console.WriteLine("Error: please provide a whole number that is 1 or larger");
+                    return false;
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error: could not parse number argument");
+            }
+            return false;
+        }
+
+        public static void PrintHelpInstructions()
+        {
+            Console.WriteLine(" ");
         }
     }
 }
--- PrimeTables/PrimeTablesTests/CLITests.cs
+++ PrimeTables/PrimeTablesTests/CLITests.cs
@@ -1,3 +1,4 @@
+using PrimeTables;
 using System;
 using Xunit;
 
@@ -5,6 +6,52 @@ namespace PrimeTablesTests
 {
     public class CLITests
     {
-       
+        [Theory]
+        [InlineData("@")]
+        [InlineData("foo")]
+        [InlineData("a")]
+        [InlineData("/a123")]
+        public void Non_Numeric_Argument_Returns_False(string argument)
+        {
+            var foo = CLI.ValidateNumberArgument(argument, out int nPrime);
+            Assert.False(foo);
+            Assert.Equal(0, nPrime);
+        }
+
+        [Fact]
+        public void Assign_Numeric_Argument_Returns_True()
+        {
+            var foo = CLI.ValidateNumberArgument("5", out int nPrime);
+            Assert.True(foo);
+            Assert.Equal(5, nPrime);
+        }
+
+        [Fact]
+        public void Assign_Negative_Numeric_Argument_Returns_False()
+        {
+            var foo = CLI.ValidateNumberArgument("-1", out int nPrime);
+            Assert.False(foo);
+            Assert.Equal(nPrime, -1);
+        }
+
+        [Fact]
+        public void Assign_Edge_Case_Numeric_Argument_Returns_True()
+        {
+            var foo = CLI.ValidateNumberArgument("2", out int nPrime);
+            Assert.True(foo);
+            Assert.Equal(2, nPrime);
+        }
+
+        [Theory]
+        [InlineData("1.23")]
+        [InlineData("5.5")]
+        [InlineData("1.5111")]
+        public void Assign_Non_Whole_Argument_Returns_False(string argument)
+        {
+            var foo = CLI.ValidateNumberArgument(argument, out int nPrime);
+            Assert.False(foo);
+            Assert.Equal(0, nPrime);
+        }
+
     }
 }
