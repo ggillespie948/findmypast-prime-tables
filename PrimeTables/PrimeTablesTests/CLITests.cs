f96fad9ea795a9c0f842259ef6ad990175f04078
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-15 14:26:39 +0100

    add test classes to reflect structure

---
+++ PrimeTables/PrimeTablesTests/CLITests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace PrimeTablesTests
+{
+    public class CLITests
+    {
+       
+    }
+}
---
+++ PrimeTables/PrimeTablesTests/PrimeNumberGeneratorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+
+namespace PrimeTablesTests
+{
+    public class PrimeNumberGeneratorTests
+    {
+       
+    }
+}
