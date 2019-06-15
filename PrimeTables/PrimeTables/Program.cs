905bb8924c220394d4b3158fad09b3b2b5762b26
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-15 13:50:41 +0100

    Init solution with new console app and xUnit test projects

---
+++ PrimeTables/PrimeTables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
