183c93b8da6d716c94e329a5a7f399130f499b0a
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-06-15 16:44:09 +0100

    refactor out stages of sieve into individual methods

--- PrimeTables/PrimeTables/PrimeNumberGenerator.cs
+++ PrimeTables/PrimeTables/PrimeNumberGenerator.cs
@@ -18,15 +18,9 @@ namespace PrimeTables
             // https://stackoverflow.com/a/51851085/9355141
             // this answer helped me with the calculation
             var maxCount = 1 + nPrimes * (Math.Log(nPrimes) + Math.Log(Math.Log(nPrimes)));
-            bool[] sieveFlags = new bool[Convert.ToInt32(maxCount) + 1];
 
-            //init sieve flags from 2 to maxCount
-            for (int i = 2; i < sieveFlags.Length; i++)
-            {
-                sieveFlags[i] = true;
-            }
-            sieveFlags[0] = false;
-            sieveFlags[1] = false;
+            bool[] sieveFlags = new bool[Convert.ToInt32(maxCount) + 1];
+            sieveFlags = InitSieveFlags(sieveFlags);
 
             //starting at the first prime number
             int prime = 2;
@@ -34,21 +28,64 @@ namespace PrimeTables
             while (prime <= 25)
             {
                 //eliminate remaining multiples of prime
-                for (int i = prime * prime; i < sieveFlags.Length; i += prime)
-                {
-                    sieveFlags[i] = false;
-                }
+                Eliminate(sieveFlags, prime);
 
                 //find the next value which hasn't been eliminated
-                int next = prime + 1;
-                while (next < sieveFlags.Length && !sieveFlags[next])
-                {
-                    next++;
-                }
-                prime = next;
+                prime = GetNextPrime(sieveFlags, prime);
             }
 
             return GenerateListOfPrimes(sieveFlags, nPrimes);
+        }
+
+
+        /// <summary>
+        /// Iterate through the list of sieve flags and 
+        /// </summary>
+        /// <param name="sieveFlags"></param>
+        /// <param name="prime"></param>
+        /// <returns></returns>
+        private static int GetNextPrime(bool[] sieveFlags, int prime)
+        {
+            int next = prime + 1;
+            while (next < sieveFlags.Length && !sieveFlags[next])
+            {
+                next++;
+            }
+            return next;
+        }
+
+
+        /// <summary>
+        /// Eliminates remaining multiples of prime, starting from (prime*prime), because anything
+        /// prior to this would have already been eliminated in a previous iteration
+        /// </summary>
+        /// <param name="primeFlags"></param>
+        /// <param name="prime"></param>
+        private static void Eliminate(bool[] primeFlags, int prime)
+        {
+            for (int i = prime * prime; i < primeFlags.Length; i += prime)
+            {
+                primeFlags[i] = false;
+            }
+        }
+
+
+        /// <summary>
+        /// Initalise the sieve flags to all be true, setting 0 + 1 to be false due to 2 being 
+        /// the first prime number
+        /// </summary>
+        /// <param name="sieveFlags"></param>
+        /// <returns></returns>
+        private static bool[] InitSieveFlags(bool[] sieveFlags)
+        {
+            //init sieve flags from 2 to maxCount
+            for (int i = 2; i < sieveFlags.Length; i++)
+            {
+                sieveFlags[i] = true;
+            }
+            sieveFlags[0] = false;
+            sieveFlags[1] = false;
+            return sieveFlags;
         }
 
 
